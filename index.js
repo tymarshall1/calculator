276ad903e49d2cef7a433d0637215907cccb7320
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-07-29 14:40:20 -0400

    created the file layout and math functions for the calculator operations

---
+++ index.js
@@ -0,0 +1,34 @@
+let operator = "";
+let firstNumber = 0;
+let secondNumber = 0;
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function operate(operator, numberOne, numberTwo) {
+  switch (operator) {
+    case "+":
+      return add(numberOne, numberTwo);
+    case "-":
+      return subtract(numberOne, numberTwo);
+    case "*":
+      return multiply(numberOne, numberTwo);
+    case "/":
+      return divide(numberOne, numberTwo);
+  }
+}
+
+console.log(operate("43", 2, 5));
