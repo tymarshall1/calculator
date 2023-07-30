cc7389f7b22fd79ee7f3bd6661cea190ba62a830
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-07-30 16:55:25 -0400

    Added in the basic calculator functionality of inputting two numbers and then clicking the equals sign to get the sum

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
-let operator = "";
-let firstNumber = 0;
-let secondNumber = 0;
+let operators = "";
+let firstNumber = "";
+let secondNumber = "";
+let displayValue = "";
+let total = "";
+
+const operation = document.querySelector("#operation");
+
+document.querySelectorAll("#number").forEach((number) =>
+  number.addEventListener("click", () => {
+    if (operators === "") {
+      firstNumber += number.textContent;
+      operation.textContent = firstNumber;
+      console.log("first", firstNumber);
+      console.log("sec", secondNumber);
+    } else {
+      secondNumber += number.textContent;
+      operation.textContent = secondNumber;
+      console.log("first", firstNumber);
+      console.log("sec", secondNumber);
+    }
+  })
+);
+
+document
+  .querySelectorAll("#operator")
+  .forEach((operator) =>
+    operator.addEventListener("click", () => (operators = operator.textContent))
+  );
+
+document
+  .querySelector("#equals")
+  .addEventListener(
+    "click",
+    () =>
+      (operation.textContent = operate(
+        operators,
+        parseInt(firstNumber),
+        parseInt(secondNumber)
+      ))
+  );
+
+document.querySelector("#clear").addEventListener("click", () => {
+  operation.textContent = "0";
+  operators = "";
+  firstNumber = "";
+  secondNumber = "";
+});
 
 function add(x, y) {
   return x + y;
@@ -30,3 +75,14 @@ function operate(operator, numberOne, numberTwo) {
       return divide(numberOne, numberTwo);
   }
 }
+
+// function updateScreen() {
+//   document.querySelectorAll("button").forEach((button) =>
+//     button.addEventListener("click", () => {
+//       displayValue += button.textContent;
+//       operation.textContent = displayValue;
+//     })
+//   );
+// }
+
+// updateScreen();
